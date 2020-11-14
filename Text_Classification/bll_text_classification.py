3abdb41eda7165d641703715bbc0b9bcbc7eaa56
Hai <65085715+bqhai@users.noreply.github.com>
2020-11-14 20:00:57 +0700

    Update new file

--- Text_Classification/bll_text_classification.py
+++ Text_Classification/bll_text_classification.py
@@ -1,12 +1,31 @@
 import os
 import pickle
 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
 from preprocessor import text_preprocess
-from train_model import label_encoder
 
 MODEL_PATH = "models"
+test_percent = 0.2
 text = []
 label = []
+
+# load label
+if not os.path.exists(MODEL_PATH):
+    os.makedirs(MODEL_PATH)
+for line in open('data/news_categories.prep', encoding="utf8"):
+    words = line.strip().split()
+    label.append(words[0])
+    text.append(' '.join(words[1:]))
+
+X_train, X_test, y_train, y_test = train_test_split(text, label, test_size=test_percent, random_state=42)
+
+# encode label
+label_encoder = LabelEncoder()
+label_encoder.fit(y_train)
+y_train = label_encoder.transform(y_train)
+y_test = label_encoder.transform(y_test)
 
 # Naive Bayes
 # model = pickle.load(open(os.path.join(MODEL_PATH,"naive_bayes.pkl"), 'rb'))
