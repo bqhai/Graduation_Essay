2492713df35a3463cbc720fc85c5c1cf93c8147c
Hai <65085715+bqhai@users.noreply.github.com>
2020-11-22 19:47:34 +0700

    add DAL, BLL, Models

---
+++ source/NewsManagementSystem/DAL_NewsManagementSystem/DAL/DAL_BlackList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL_NewsManagementSystem.Models;
+
+namespace DAL_NewsManagementSystem.DAL
+{
+    public class DAL_BlackList
+    {
+        NewsManagementSystemEntities db = new NewsManagementSystemEntities();
+        public DAL_BlackList()
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+        }
+        
+    }
+}
---
+++ source/NewsManagementSystem/Models_NewsManagementSystem/Repository/EntityMapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using DAL_NewsManagementSystem.Models;
+
+namespace Models_NewsManagementSystem.Repository
+{
+    public class EntityMapper<TSource, TDestination> where TSource:class where TDestination:class
+    {
+        public EntityMapper()
+        {
+            
+        }
+        public TDestination Translate(TSource obj)
+        {
+            return Mapper.Map<TDestination>(obj);
+        }
+    }
+}
