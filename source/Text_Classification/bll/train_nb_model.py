554d32a63c7a8f988673bab8dd3d484dff8209e3
Hai <65085715+bqhai@users.noreply.github.com>
2020-12-02 22:50:07 +0700

    remove unnecessary

--- source/Text_Classification/bll/train_nb_model.py
+++ source/Text_Classification/bll/train_nb_model.py
@@ -24,14 +24,6 @@ for line in open('../data/news_categories.prep', encoding='utf8'):
 
 X_train, X_test, y_train, y_test = train_test_split(text, label, test_size=test_percent, random_state=42)
 
-# Save train/test data
-# with open('data/train.txt', 'w', encoding="utf8") as fp:
-#     for x, y in zip(X_train, y_train):
-#         fp.write('{} {}\n'.format(y, x))
-#
-# with open('data/test.txt', 'w', encoding="utf8") as fp:
-#     for x, y in zip(X_test, y_test):
-#         fp.write('{} {}\n'.format(y, x))
 
 # encode label
 label_encoder = LabelEncoder()
