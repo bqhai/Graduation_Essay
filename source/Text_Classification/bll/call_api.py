6e7c40217f140412d48e3c86e87a6a9396a41fdd
Bui Nguyen Quang Hai <65085715+bqhai@users.noreply.github.com>
2020-12-11 16:06:08 +0700

    Update call_api.py

--- source/Text_Classification/bll/call_api.py
+++ source/Text_Classification/bll/call_api.py
@@ -24,7 +24,57 @@ def add_list_json_post(data):
         return -2
 
 
-def get_all_black_list():
+def check_exist_post(post_url_id):
+    try:
+        url = BASE_LOCAL_URL + 'api/Home/CheckExistPost/' + post_url_id + '/'
+        logging.info('Call to api ' + url)
+        response = requests.get(url,  verify=False)
+        print('Status code: ', response.status_code)
+        logging.info('Status code: ' + str(response.status_code))
+        logging.info('Call api successfully')
+        return response.json()
+    except:
+        logging.error('Call api failed')
+        return -2
+
+
+def add_json_post(data):
+    try:
+        url = BASE_LOCAL_URL + 'api/Home/AddNewPost/'
+        logging.info('Call to api ' + url)
+        response = requests.post(url, json=data, verify=False)
+        print('Status code: ', response.status_code)
+        logging.info('Status code: ' + str(response.status_code))
+        logging.info('Call api successfully')
+        print(response.text)
+        if response.text == 'true':
+            return 0
+        else:
+            return -4
+    except:
+        logging.error('Call api failed')
+        return -2
+
+
+def update_json_post(data):
+    try:
+        url = BASE_LOCAL_URL + 'api/Home/UpdatePost/'
+        logging.info('Call to api ' + url)
+        response = requests.post(url, json=data, verify=False)
+        print('Status code: ', response.status_code)
+        logging.info('Status code: ' + str(response.status_code))
+        logging.info('Call api successfully')
+        print(response.text)
+        if response.text == 'true':
+            return 0
+        else:
+            return -4
+    except:
+        logging.error('Call api failed')
+        return -2
+
+
+def get_all_watch_list():
     try:
         url = BASE_LOCAL_URL + 'api/Home/GetAllWatchList/'
         logging.info('Call to api ' + url)
