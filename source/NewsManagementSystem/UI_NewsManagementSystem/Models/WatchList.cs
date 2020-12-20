dd3390de7fbd6eadcd3be5f15d7fa65c4f743558
Hai <65085715+bqhai@users.noreply.github.com>
2020-12-21 00:24:59 +0700

    Create WatchList.cs

---
+++ source/NewsManagementSystem/UI_NewsManagementSystem/Models/WatchList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UI_NewsManagementSystem.Models
+{
+    public class WatchList
+    {
+        public string FacebookID { get; set; }
+        public string FacebookName { get; set; }
+        public string FacebookUrl { get; set; }
+        public string FacebookTypeID { get; set; }
+        public string FacebookTypeName { get; set; }
+        public bool Status { get; set; }
+        public bool InBlackList { get; set; }
+    }
+}
+}
