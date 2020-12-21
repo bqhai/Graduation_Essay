7278bcead3986a158449c09a33be9047e0374c7e
Hai <65085715+bqhai@users.noreply.github.com>
2020-12-21 20:20:47 +0700

    Update LoginController.cs

--- source/NewsManagementSystem/UI_NewsManagementSystem/Controllers/LoginController.cs
+++ source/NewsManagementSystem/UI_NewsManagementSystem/Controllers/LoginController.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UI_NewsManagementSystem.Models;
+using UI_NewsManagementSystem.Repository;
 
 namespace UI_NewsManagementSystem.Controllers
 {
     public class LoginController : Controller
     {
+        private ApiService _apiService = new ApiService();
         // GET: Login
         public ActionResult Login()
         {
             return View();
+        }
+        public ActionResult ProcessLogin(Account account)
+        {
+            var response = _apiService.GetResponse("api/Login/ProcessLogin/" + account.Username + "/" + account.Password + "/");
+            if (response.IsSuccessStatusCode)
+            {
+
+            }
         }
     }
 }
