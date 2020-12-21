db74fe92d1df61f0987a66412c4fae3009e6e07f
Hai <65085715+bqhai@users.noreply.github.com>
2020-12-22 03:48:57 +0700

    Update HomeController.cs

--- source/NewsManagementSystem/UI_NewsManagementSystem/Controllers/HomeController.cs
+++ source/NewsManagementSystem/UI_NewsManagementSystem/Controllers/HomeController.cs
@@ -1,16 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
+using UI_NewsManagementSystem.Models;
+using UI_NewsManagementSystem.Repository;
+using PagedList;
 
 namespace UI_NewsManagementSystem.Controllers
 {
     public class HomeController : Controller
     {
+        private ApiService _apiService = new ApiService();
         public ActionResult Index()
         {
             return View();
         }
+        #region WatchList
+        public ActionResult WatchList(int pageIndex = 1, int pageSize = 13)
+        {
+            var response = _apiService.GetResponse("api/Home/GetAllWatchList");
+            if (response.IsSuccessStatusCode)
+            {
+                var result = response.Content.ReadAsAsync<List<WatchList>>().Result;
+                return View(result.ToPagedList(pageIndex, pageSize));
+            }
+            return RedirectToAction("Index", "Home");
+        }
+        #endregion
     }
 }
