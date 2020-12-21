7394aa078ee5863c534ac87575ffb15a00a5ac90
Hai <65085715+bqhai@users.noreply.github.com>
2020-12-21 21:17:08 +0700

    Update LoginController.cs

--- source/NewsManagementSystem/UI_NewsManagementSystem/Controllers/LoginController.cs
+++ source/NewsManagementSystem/UI_NewsManagementSystem/Controllers/LoginController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 using UI_NewsManagementSystem.Models;
@@ -21,8 +22,19 @@ namespace UI_NewsManagementSystem.Controllers
             var response = _apiService.GetResponse("api/Login/ProcessLogin/" + account.Username + "/" + account.Password + "/");
             if (response.IsSuccessStatusCode)
             {
-
+                var resultLogin = response.Content.ReadAsAsync<bool>().Result;
+                if (resultLogin)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    TempData["DangerMessage"] = Message.AuthenticateFail();
+                    return Redirect(this.Request.UrlReferrer.ToString());
+                }
             }
+            TempData["DangerMessage"] = Message.ConnectFail();
+            return Redirect(this.Request.UrlReferrer.ToString());
         }
     }
 }
