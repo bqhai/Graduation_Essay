3855d91dd3a0693a8bdcf14618bf2a3f8744ca0f
Hai <65085715+bqhai@users.noreply.github.com>
2020-12-21 20:20:40 +0700

    Create Message.cs

---
+++ source/NewsManagementSystem/UI_NewsManagementSystem/Repository/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UI_NewsManagementSystem.Repository
+{
+    public class Message
+    {
+        public static string ConnectFail()
+        {
+            return "Kết nối server thất bại";
+        }
+        public static string AuthenticateFail()
+        {
+            return "Tài khoản hoặc mật khẩu không chính xác";
+        }
+    }
+}
