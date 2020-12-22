efdae296537af4e5746a9a02af26cd5d812ad4db
Hai <65085715+bqhai@users.noreply.github.com>
2020-12-23 02:21:37 +0700

    Update Message.cs

--- source/NewsManagementSystem/UI_NewsManagementSystem/Repository/Message.cs
+++ source/NewsManagementSystem/UI_NewsManagementSystem/Repository/Message.cs
@@ -7,13 +7,29 @@ namespace UI_NewsManagementSystem.Repository
 {
     public class Message
     {
-        public static string ConnectFail()
+        public static string ConnectFailed()
         {
             return "Kết nối server thất bại";
         }
-        public static string AuthenticateFail()
+        public static string AuthenticateFailed()
         {
             return "Tài khoản hoặc mật khẩu không chính xác";
+        }
+        public static string AddItemSuccessful()
+        {
+            return "Thêm thành công";
+        }
+        public static string AddItemFaled()
+        {
+            return "Thêm thất bại, có lỗi xảy ra phía server";
+        }
+        public static string UpdateItemSuccessful()
+        {
+            return "Cập nhật thành công";
+        }
+        public static string UpdateItemFaled()
+        {
+            return "Cập nhật thất bại, có lỗi xảy ra phía server";
         }
     }
 }
