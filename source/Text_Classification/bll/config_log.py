6968580f8cf0e30d97845f54f53e6f8e74a68692
Hai <65085715+bqhai@users.noreply.github.com>
2020-12-25 19:59:18 +0700

    Update config_log.py

--- source/Text_Classification/bll/config_log.py
+++ source/Text_Classification/bll/config_log.py
@@ -2,5 +2,5 @@ import logging
 
 
 log_format = '%(asctime)s.%(msecs)03d ''|%(levelname)s|\t''%(filename)s:''%(funcName)s():''%(lineno)d ''--%(message)s'
-logging.basicConfig(level=logging.INFO, filename='../log/run_time.log', filemode='a', format=log_format, datefmt='%d/%m/%Y %H:%M:%S')
+logging.basicConfig(level=logging.INFO, filename='log/run_time.log', filemode='a', format=log_format, datefmt='%d/%m/%Y %H:%M:%S')
 
