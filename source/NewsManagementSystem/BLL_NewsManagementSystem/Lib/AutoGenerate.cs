f4adb1c7707266826a95c19d3fe7712a9f0d030b
Hai <65085715+bqhai@users.noreply.github.com>
2021-01-07 05:41:51 +0700

    Update AutoGenerate.cs

--- source/NewsManagementSystem/BLL_NewsManagementSystem/Lib/AutoGenerate.cs
+++ source/NewsManagementSystem/BLL_NewsManagementSystem/Lib/AutoGenerate.cs
@@ -9,10 +9,17 @@ namespace BLL_NewsManagementSystem.Lib
 {
     public static class AutoGenerate
     {
+        private static Random random = new Random();
         public static string PostID()
         {
             Thread.Sleep(5);
-            return DateTime.Now.ToString("ddMMyyyyHHmmssfffff");
+            return DateTime.Now.ToString("ddMMyyyyHHmmss") + RandomString(6);
+        }
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
         }
     }
 }
