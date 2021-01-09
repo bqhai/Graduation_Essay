dd0ef5ee46c1f858828612bfa3d7ed2e4370bc64
Hai <65085715+bqhai@users.noreply.github.com>
2021-01-10 01:07:32 +0700

    Update common.py

--- source/Text_Classification/bll/common.py
+++ source/Text_Classification/bll/common.py
@@ -1,5 +1,5 @@
 __author__ = 'Hai Bui'
 
 BASE_LOCAL_URL = 'https://localhost:44347/'
-BASE_HOSTED_URL = 'http://kltn262020api.somee.com/'
+BASE_HOSTED_URL = 'http://kltn26lv2020api.somee.com/'
 MODEL_PATH = '../models'
